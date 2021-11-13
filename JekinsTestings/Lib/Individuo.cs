d4f926802bec53e778d2fe6e4966223155966d53
Sergio Fontes <sergio@computencial.com>
2021-11-13 10:34:33 +0000

    Primeira versão do projecto

---
+++ JekinsTestings/Lib/Individuo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lib
+{
+    public class Individuo
+    {
+        public string Nome { get; set; }
+        public string Apelido { get; set; }
+        public DateTime DataNascimento { get; set; }
+
+        public string NomeCompleto { 
+            get
+            {
+                return string.Format("{0} {1}", this.Nome, this.Apelido);
+            }
+        }
+        public string GetNomeCompleto()
+        {
+            return string.Format("{0} {1}", this.Nome, this.Apelido);
+        }
+        public int GetIdade()
+        {
+            int difyears = DateTime.Now.Year - DataNascimento.Year;
+            return difyears;
+        }
+    }
+}
---
+++ JekinsTestings/MsTest/UnitTest1.cs
@@ -0,0 +1,51 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lib;
+namespace MsTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod, TestCategory("CategoriaV"), Priority(1)]
+        public void Teste_NomeCompleto_Verdadeiro()
+        {
+            Individuo obj = new Individuo();
+            obj.Nome = "Sérgio";
+            obj.Apelido = "Fontes";
+
+            Assert.IsTrue(obj.GetNomeCompleto() == "Sérgio Fontes");
+        }
+        [TestMethod , TestCategory("CategoriaF"), Priority(1)]
+        public void Teste_NomeCompleto_Falso()
+        {
+            Individuo obj = new Individuo();
+            obj.Nome = "Sérgio";
+            obj.Apelido = "Fontes";
+
+            Assert.IsFalse(obj.GetNomeCompleto() == "Sandrio Fontes");
+        }
+
+        [TestMethod, TestCategory("CategoriaV"), Priority(2)]
+        [DataRow("João","Silva","João Silva")]
+        [DataRow("Pedro", "Sequeira", "Pedro Sequeira")]
+        public void Teste_NomeCompleto_Verdadeiro2(string nome, string apelido,string resultado)
+        {
+            Individuo obj = new Individuo();
+            obj.Nome = nome;
+            obj.Apelido = apelido;
+
+            Assert.IsTrue(obj.GetNomeCompleto() == resultado);
+        }
+
+        [TestMethod, TestCategory("CategoriaF"), Priority(2)]
+        [DataRow("João", "Silva", "João José")]
+        [DataRow("Pedro", "Sequeira", "Pedro Antunes")]
+        public void Teste_NomeCompleto_Falso2(string nome, string apelido, string resultado)
+        {
+            Individuo obj = new Individuo();
+            obj.Nome = nome;
+            obj.Apelido = apelido;
+
+            Assert.IsTrue(obj.GetNomeCompleto() == resultado);
+        }
+    }
+}
